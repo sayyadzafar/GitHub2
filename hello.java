581ce78d0c87d8c52d6f634b74394ee69be2003d
sayyadzafar@ymail.com <33867008+sayyadzafar@users.noreply.github.com>
2017-11-27 15:00:25 +0530

    modified program 2

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ public class hello{
    public static void main(String[] args){
           
 		  for(int i=1;i<=10;i++){
-		        System.out.println("Hello World1 ..."+i);
+		        System.out.println("Hello World12 ..."+i);
 		  }
    }
 
