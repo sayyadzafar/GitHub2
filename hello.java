cefceb1d6aefb7603ae9750d38bd1ff2aa2afdf2
sayyadzafar@ymail.com <33867008+sayyadzafar@users.noreply.github.com>
2017-11-29 17:00:48 +0530

    mnmn

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ public class hello{
    public static void main(String[] args){
           
 		  for(int i=1;i<=10;i++){
-		        System.out.println("Hello World34 ..."+i);
+		        System.out.println("Hello World35 ..."+i);
 		  }
    }
 
