e19d50ff07e35d7020efcb15c21876af6dca2996
sayyadzafar@ymail.com <33867008+sayyadzafar@users.noreply.github.com>
2017-11-29 17:24:59 +0530

    jhk

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ public class hello{
    public static void main(String[] args){
           
 		  for(int i=1;i<=10;i++){
-		        System.out.println("Hello World41 ..."+i);
+		        System.out.println("Hello World42 ..."+i);
 		  }
    }
 
